4a0ec3b4d79f502c848eb82964a352a476b90a53
InEase <inease28@gmail.com>
2021-11-29 12:21:36 +0800

    v0.3.2 release:we need to reconsider the import methods
    Signed-off-by: InEase <inease28@gmail.com>

--- TorchMiner/__init__.py
+++ TorchMiner/__init__.py
@@ -1,6 +1,5 @@
 from TorchMiner.plugins.Plugin import BasePlugin
 from .miner import Miner
-from .plugins import *
 
 __version__ = "0.3.1"
 
--- TorchMiner/plugins/__init__.py
+++ TorchMiner/plugins/__init__.py
@@ -1,4 +1,1 @@
-from .Drawer import *
-from .Logger import *
 from .Plugin import *
-from .Sheet import *
