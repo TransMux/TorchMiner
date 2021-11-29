3c36ee40d45ced2393ce95e8b2c2d45d25931daf
InEase <inease28@gmail.com>
2021-11-29 12:19:41 +0800

    v0.3.1 release:bug fix
    Signed-off-by: InEase <inease28@gmail.com>

--- TorchMiner/__init__.py
+++ TorchMiner/__init__.py
@@ -2,6 +2,6 @@ from TorchMiner.plugins.Plugin import BasePlugin
 from .miner import Miner
 from .plugins import *
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = ["Miner", "BasePlugin"]
