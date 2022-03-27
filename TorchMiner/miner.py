f87343e01bb34888040933177333056d482e83bb
InEase <InEase28@gmail.com>
2022-03-28 00:30:52 +0800

    Trying to make all things plugin

--- TorchMiner/miner.py
+++ TorchMiner/miner.py
@@ -1,202 +1,41 @@
 from pathlib import Path
+from typing import List
 
 import torch
 import tqdm
+from TorchMiner import BasePlugin
 from torch.optim import Optimizer
 
 from TorchMiner.Logger import ColoredLogger
 from TorchMiner.plugins import PluginManager
 from . import utils
-from .utils import find_resume_target
+from TorchMiner.utils import find_resume_target, TorchMinerSettings, TorchMinerMetrics
 
 
 class Miner(object):
     def __init__(
             self,
-            alchemy_directory,
-            experiment,
-            model,
-            optimizer,
-            loss_func,
-            train_dataloader=None,
-            val_dataloader=None,
-            resume=True,
-            eval_epoch=1,
-            persist_epoch=1,
-            gpu=True,
-            max_epochs=9999999,
-            in_notebook=False,
-            plugins=None,
-            accumulated_iter=1,
-            ignore_optimizer_resume=False,
-            amp=False,
-            amp_scaler=True,
+            setting: TorchMinerSettings,
+            plugins: List[BasePlugin],
     ):
         """
-        Core Of TorchMiner
-        :param alchemy_directory: The directory which TorchMiner will use to Store Everything in
-        :param torch.nn.Module model: Target
-        :param torch.optim.Optimizer: One should promise that Optimizer is inited on same device or
-         a function that accepts model and returns an Optimizer, and TorchMiner will create the optimizer from it
-        :param loss_func: A function to compute Loss
-            A Special Function, the function receives 2 variable:
-            * Miner: The Miner Object
-            * Data: The Batch data yield by the loader
-            return Value should be a float number of the loss.
-        :param string experiment: Experiment Name
-        :param torch.utils.data.DataLoader train_dataloader:
 
-            --- Optional ---
-        :param torch.utils.data.DataLoader val_dataloader: Default None. If None, skip Validation
-        :param bool resume: Default True.
-        :param int eval_epoch: Default 1. Validate every 'eval_epoch'
-        :param int persist_epoch: Default 1. Save model every 'persist_epoch'
-        :param gpu:
+        :param setting:
         :param plugins:
-            The Differences between Hooks and Plugins:
-                Hooks are Functions,They receive Miner and Payloads
-                 - Each Hook can load One Function Each Time
-                Plugins are Classes succeed to `TorchMiner.Plugin`
-                 - Receives Miner when Plugin Inits
-                 - Receives only payload when Hooks in Plugin was called
-                 - One can use many plugins in a miner
-        :param max_epochs:
-        :param in_notebook:
-        :param accumulated_iter:
-        :param ignore_optimizer_resume:
-        :param amp:
-        :param amp_scaler:
         """
-        self.alchemy_directory: Path = Path(alchemy_directory)  # working dir
-        self.experiment: str = experiment
+        self.settings: TorchMinerSettings = setting
+        self.metrics = TorchMinerMetrics()
+        # After read in
         self._create_dirs()
-        self.devices = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.experiment_dir = alchemy_directory / self.experiment
-        self.models_dir = alchemy_directory / self.experiment / "models"
-        self.accumulated_iter = float(accumulated_iter)
-        self.model = model.to(self.devices)
-        self.train_dataloader = train_dataloader
-        self.logger_prototype = ColoredLogger
-        self.val_dataloader = val_dataloader
-        self.gpu = gpu
-        self.in_notebook = in_notebook
-        self.ignore_optimizer_resume = ignore_optimizer_resume
-
-        if isinstance(optimizer, Optimizer):
-            self.optimizer = optimizer
-        else:
-            self.optimizer = optimizer(self.model)
-        self.loss_func = loss_func.to(self.devices)
-        self.resume = resume
-        self.eval_epoch = eval_epoch
-        self.persist_stride = persist_epoch
-        self.lowest_train_loss = float("inf")
-        self.lowest_val_loss = float("inf")
-        self.current_epoch = 0
-        self.current_train_iteration = 0
-        self.current_val_iteration = 0
-        self.max_epochs = max_epochs
-        self.amp = amp
-        self.amp_scaler = amp_scaler
-        if self.amp and self.amp_scaler:
-            self.scaler = torch.cuda.amp.GradScaler()
-        self.tqdm = tqdm.tqdm
-
-        self.train_only = False if val_dataloader else True
         # --- Init Plugin ---
         self.plugins = PluginManager(self, plugins)
-        self.logger = self.get_logger("Miner")
+        self.logger = self.settings.get_logger("Miner")
         # --- Before Init ---
-        self.status = "init"
         self.plugins.call("before_init")
-        self._init_model()
+        self.settings.prepare(self)
+        self.metrics.prepare(self)
         # --- After Init ---
         self.plugins.call("after_init")
-        if self.train_only:
-            self.logger.info("Running in Train Only Mode")
-
-    @staticmethod
-    def get_logger(name):
-        return ColoredLogger(name)
-
-    def _init_model(self):
-        """resume from some checkpoint"""
-        if isinstance(self.model, torch.nn.DataParallel):
-            raise Exception(
-                "Don't parallel the model yourself, instead, if the "
-                "`gpu` option is true(default), TorchMiner will do this for you."
-            )
-        if self.resume:
-            check_point = find_resume_target(self.models_dir, self.resume)
-            if check_point:
-                self.logger.info(f"Start to load checkpoint {check_point}")
-                self._resume(check_point)
-            else:
-                self.logger.warning("Could not find checkpoint to resume, " "train from scratch")
-
-        self.model = self._parallel_model(self.model)
-
-    def _parallel_model(self, model):
-        # TODO:统一 miner.gpu 和 miner.device 的设置
-        # TODO:探索模型平行的原理，如何完成，数据集需要吗
-        if self.gpu:
-            gpu_count = torch.cuda.device_count()
-            if gpu_count == 0:
-                self.logger.warning("no GPU detected, will train on CPU.")
-            else:
-                self.logger.info(f"found {gpu_count} GPUs, will use all of them to train")
-                devices = list(range(gpu_count))
-                model.cuda()
-                model = torch.nn.DataParallel(model, devices)
-        return model
-
-    def _resume(self, checkpoint_path):
-        # TODO:After Loading Checkpoint, output basic information
-        checkpoint = torch.load(checkpoint_path)
-
-        # load model state
-        try:
-            self.model.load_state_dict(checkpoint["state_dict"], strict=True)
-        except Exception as e:
-            self.logger.warning(e)
-            self.logger.critical(
-                f"load checkpoint failed, the state in the "
-                "checkpoint is not matched with the model, "
-                "try to reload checkpoint with unstrict mode"
-            )
-            # UnStrict Mode
-            self.model.load_state_dict(checkpoint["state_dict"], strict=False)
-
-        # load optimizer state
-        if "optimizer" in checkpoint and not self.ignore_optimizer_resume:
-            try:
-                self.optimizer.load_state_dict(checkpoint["optimizer"])
-            except Exception as e:
-                self.logger.warning(e)
-                self.logger.critical(
-                    f"load optimizer state failed, will skip this error and continue, "
-                    "stop the process if it is not expected"
-                )
-        # Read Train Process From Resumed Data
-        self.current_epoch = checkpoint.get("epoch", 0)
-        self.current_train_iteration = checkpoint.get("train_iteration", 0)
-        self.current_val_iteration = checkpoint.get("val_iteration", 0)
-        self.lowest_train_loss = checkpoint.get("lowest_train_loss", 9999)
-        self.lowest_val_loss = checkpoint.get("lowest_val_loss", 9999)
-
-        # load scaler state
-        if self.amp and self.amp_scaler:
-            try:
-                self.scaler.load_state_dict(checkpoint["scaler"])
-            except Exception as e:
-                self.logger.warning(
-                    f"load scaler state failed with {e}, will skip this error and continue, "
-                    "stop the process if it is not expected"
-                )
-
-        self.plugins.load(checkpoint)
-
-        self.logger.info(f"Checkpoint {checkpoint_path} Successfully Loaded")
 
     # else:
 
@@ -205,33 +44,30 @@ class Miner(object):
         Core Function:start to train the model
         :return:
         """
-        while True:
-            self.current_epoch += 1
-            self.plugins.call("before_train_epoch_start", epoch=self.current_epoch)
-            self.model.train()  # Set Train Mode
-            train_iters = len(self.train_dataloader)  # For future change during training
 
-            total_train_loss = 0
-            self.logger.info(f"start to train epoch {self.current_epoch}")
-            t = self.tqdm(self.train_dataloader)
+        while True:
+            # TODO: can we make a shortcut for metrics?
+            self.metrics.current_epoch += 1
+            self.plugins.call("before_train_epoch_start")
+            self.settings.model.train()  # Set Train Mode
+            self.metrics.train_iters = len(self.settings.train_dataloader)  # For future change during training
+            self.metrics.total_train_loss = 0
+            self.logger.info(f"start to train epoch {self.metrics.current_epoch}")
+            t = self.settings.tqdm(self.settings.train_dataloader)
+
             for index, data in enumerate(t):
-                self.plugins.call(
-                    "before_train_iteration_start",
-                    data=data,
-                    index=index,
-                    total_iters=train_iters,
-                    iteration=self.current_train_iteration,
-                )
-                train_loss = self._run_train_iteration(data)
-                self.plugins.call(
-                    "after_train_iteration_end",
-                    loss=train_loss,
-                    data=data,
-                    index=index,
-                    total_iters=train_iters,
-                    iteration=self.current_train_iteration,
-                )
-                t.set_postfix({"train loss": train_loss})
+                self.metrics.data = data
+                self.metrics.data_index = index
+
+                self.plugins.call("before_train_iteration_start")
+                self.metrics.train_loss = self._run_train_iteration(data)
+                t.set_postfix({"train loss": self.metrics.train_loss})
+
+                self.metrics["total_train_loss"] += self.metrics["train_loss"]
+
+                self.plugins.call("after_train_iteration_end")
+
+                # TODO: simplify amp procedure
                 if int((index + 1) % self.accumulated_iter) == 0:
                     if self.amp and self.amp_scaler:
                         self.scaler.step(self.optimizer)
@@ -242,27 +78,22 @@ class Miner(object):
                         self.optimizer.zero_grad()
                     else:
                         self.optimizer.zero_grad(set_to_none=True)
-                total_train_loss += train_loss
             # DataLoader End
             if self.amp and self.amp_scaler:
                 self.optimizer.zero_grad()
             else:
                 self.optimizer.zero_grad(set_to_none=True)
 
-            total_train_loss = total_train_loss / train_iters
+            self.metrics['total_train_loss'] = self.metrics["total_train_loss"] / self.metrics["train_iters"]
             self.logger.info(
-                f"training of epoch {self.current_epoch} finished, "
-                f"loss is {total_train_loss}"
+                f"training of epoch {self.metrics['current_epoch']} finished, "
+                f"loss is {self.metrics['total_train_loss']}"
             )
 
-            self.plugins.call(
-                "after_train_epoch_end",
-                train_loss=total_train_loss,
-                epoch=self.current_epoch,
-            )
+            self.plugins.call("after_train_epoch_end")
 
             # Begin eval
-            if not self.current_epoch % self.eval_epoch and self.val_dataloader:
+            if not self.metrics["current_epoch"] % self.eval_epoch and self.val_dataloader:
                 self.plugins.call(
                     "before_val_epoch_start",
                     epoch=self.current_epoch,
@@ -417,3 +248,4 @@ class Miner(object):
             utils.create_dir(self.alchemy_directory)
             utils.create_dir(self.alchemy_directory, self.experiment)
             utils.create_dir(self.alchemy_directory, self.experiment, "models")
+
--- TorchMiner/utils.py
+++ TorchMiner/utils.py
@@ -1,7 +1,13 @@
 import io
+import logging
 from pathlib import Path
 
 import numpy as np
+import torch
+import tqdm
+
+from TorchMiner.Logger import ColoredLogger
+from torch.optim import Optimizer
 
 
 def seed_everything(seed):
@@ -86,3 +92,193 @@ def find_resume_target(path: Path, index):
             return path
     print(f"Tried to find Checkpoint in f{search_paths} but failed.")
     return None
+
+
+class TorchMinerSettings:
+    def __init__(
+            self,
+            alchemy_directory,
+            experiment,
+            model,
+            optimizer,
+            loss_func,
+            train_dataloader=None,
+            val_dataloader=None,
+            resume=True,
+            eval_epoch=1,
+            persist_epoch=1,
+            gpu=True,
+            max_epochs=9999999,
+            in_notebook=False,
+            accumulated_iter=1,
+            ignore_optimizer_resume=False,
+            amp=False,
+            amp_scaler=True,
+    ):
+        """
+        Core Of TorchMiner
+        :param alchemy_directory: The directory which TorchMiner will use to Store Everything in
+        :param torch.nn.Module model: Target
+        :param torch.optim.Optimizer: One should promise that Optimizer is inited on same device or
+         a function that accepts model and returns an Optimizer, and TorchMiner will create the optimizer from it
+        :param loss_func: A function to compute Loss
+            A Special Function, the function receives 2 variable:
+            * Miner: The Miner Object
+            * Data: The Batch data yield by the loader
+            return Value should be a float number of the loss.
+        :param string experiment: Experiment Name
+        :param torch.utils.data.DataLoader train_dataloader:
+
+            --- Optional ---
+        :param torch.utils.data.DataLoader val_dataloader: Default None. If None, skip Validation
+        :param bool resume: Default True.
+        :param int eval_epoch: Default 1. Validate every 'eval_epoch'
+        :param int persist_epoch: Default 1. Save model every 'persist_epoch'
+        :param gpu:
+
+        :param max_epochs:
+        :param in_notebook:
+        :param accumulated_iter:
+        :param ignore_optimizer_resume:
+        :param amp:
+        :param amp_scaler:
+        """
+        self.miner = None
+        self.devices = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # Paths
+        self.alchemy_directory = Path(alchemy_directory)  # working dir
+        self.experiment = experiment
+        self.experiment_dir = alchemy_directory / experiment
+        self.models_dir = alchemy_directory / experiment / "models"
+        # Model
+        self.model = model.to(self.devices)
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.optimizer = optimizer if isinstance(optimizer, Optimizer) else optimizer(model)
+        self.loss_func = loss_func.to(self.devices)
+        self.amp = amp
+        self.amp_scaler = amp_scaler
+        self.scaler = None
+        # setting
+        self.resume = resume
+        self.eval_epoch = eval_epoch
+        self.persist_stride = persist_epoch
+        self.train_only = False if val_dataloader else True
+        self.accumulated_iter = float(accumulated_iter)
+        self.gpu = gpu
+        self.in_notebook = in_notebook
+        self.ignore_optimizer_resume = ignore_optimizer_resume
+        self.max_epochs = max_epochs
+        if amp and amp_scaler:
+            self.scaler = torch.cuda.amp.GradScaler()
+        # Others
+        self.logger_prototype = ColoredLogger
+        self.tqdm = tqdm.tqdm
+        self.logger = None
+        # if self.train_only:
+        #     self.logger.info("Running in Train Only Mode")
+
+    def get_logger(self, name) -> logging.Logger:
+        return self.logger_prototype(name)
+
+    def prepare(self, miner):
+        self.miner = miner
+        self.logger = self.get_logger("Setting")
+        self._parallel_model()
+
+    def _parallel_model(self):
+        if isinstance(self.model, torch.nn.DataParallel):
+            # TODO: Cut GPU option, decide it by TorchMiner
+            raise Exception(
+                "Don't parallel the model yourself, instead, if the "
+                "`gpu` option is true(default), TorchMiner will do this for you."
+            )
+        # TODO:统一 miner.gpu 和 miner.device 的设置
+        # TODO:探索模型平行的原理，如何完成，数据集需要吗
+        if self.gpu:
+            gpu_count = torch.cuda.device_count()
+            if gpu_count == 0:
+                self.logger.warning("no GPU detected, will train on CPU.")
+            else:
+                self.logger.info(f"found {gpu_count} GPUs, will use all of them to train")
+                devices = list(range(gpu_count))
+                self.model.cuda()
+                # TODO: 把Cuda移出去
+                self.model = torch.nn.DataParallel(self.model, devices)
+
+
+class TorchMinerMetrics:
+    def __init__(self):
+        self.miner = None
+        self.logger = None
+
+        self.lowest_train_loss = float("inf")
+        self.lowest_val_loss = float("inf")
+        self.current_epoch = 0
+        self.current_train_iteration = 0
+        self.current_val_iteration = 0
+        #     In Epoch metrics
+        self.train_iters = 0
+        self.total_train_loss = 0
+        self.data = None
+        self.data_index = 0
+        self.train_loss = 0
+
+    def prepare(self, miner):
+        self.logger = self.miner.settings.get_logger("Metrics")
+        self.miner = miner
+        self._resume()
+
+    def _resume(self):
+        setting: TorchMinerSettings = self.miner.settings
+        check_point = find_resume_target(setting.models_dir, setting.resume)
+        if check_point:
+            # === Start Resume Procedure ===
+            self.logger.info(f"Start to load checkpoint {check_point}")
+            # TODO:After Loading Checkpoint, output basic information
+            checkpoint = torch.load(check_point)
+            # load model state
+            try:
+                setting.model.load_state_dict(checkpoint["state_dict"], strict=True)
+            except Exception as e:
+                self.logger.warning(e)
+                self.logger.critical(
+                    f"load checkpoint failed, the state in the "
+                    "checkpoint is not matched with the model, "
+                    "try to reload checkpoint with unstrict mode"
+                )
+                # UnStrict Mode
+                setting.model.load_state_dict(checkpoint["state_dict"], strict=False)
+
+            # load optimizer state
+            if "optimizer" in checkpoint and not setting.ignore_optimizer_resume:
+                try:
+                    setting.optimizer.load_state_dict(checkpoint["optimizer"])
+                except Exception as e:
+                    self.logger.warning(e)
+                    self.logger.critical(
+                        f"load optimizer state failed, will skip this error and continue, "
+                        "stop the process if it is not expected"
+                    )
+            # Read Train Process From Resumed Data
+            self.current_epoch = checkpoint.get("epoch", 0)
+            self.current_train_iteration = checkpoint.get("train_iteration", 0)
+            self.current_val_iteration = checkpoint.get("val_iteration", 0)
+            self.lowest_train_loss = checkpoint.get("lowest_train_loss", 9999)
+            self.lowest_val_loss = checkpoint.get("lowest_val_loss", 9999)
+
+            # load scaler state
+            if setting.amp and setting.amp_scaler:
+                try:
+                    setting.scaler.load_state_dict(checkpoint["scaler"])
+                except Exception as e:
+                    self.logger.warning(
+                        f"load scaler state failed with {e}, will skip this error and continue, "
+                        "stop the process if it is not expected"
+                    )
+
+            self.miner.plugins.load(checkpoint)
+
+            self.logger.info(f"Checkpoint {checkpoint} Successfully Loaded")
+        else:
+            self.logger.warning("Could not find checkpoint to resume, " "train from scratch")
